b25c3a122e56d28c3d6ec9709abece96a4ef08b7
tadm123 <drb_40@hotmail.com>
2023-10-15 02:23:04 -0400

    Create io.cpp

---
+++ Ch2/io.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "io.h"
+
+
+int readNumber()
+{
+    std::cout << "Enter a number to add: ";
+    int x {};
+    std::cin >> x;
+    return x;
+}
+
+void writeAnswer(int x)
+{
+    std::cout << "The answer is " << x << '\n';
+}
