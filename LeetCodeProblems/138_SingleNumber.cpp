33010c8ae89d6d48275df648a808a9aeb3973ec4
tadm123 <drb_40@hotmail.com>
2024-10-11 01:19:16 -0400

    Create 138_SingleNumber.cpp

---
+++ LeetCodeProblems/138_SingleNumber.cpp
@@ -0,0 +1,18 @@
+/*
+Single Number:
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+*/
+
+class Solution {
+public:
+    int singleNumber(std::vector<int>& nums) 
+    {
+        int result{0};                  //n^0 = n
+        for (auto const& digit: nums)
+        {
+            result = digit^result;
+        }
+        return result;
+    }
+};
