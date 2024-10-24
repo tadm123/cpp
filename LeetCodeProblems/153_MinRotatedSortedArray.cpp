62daf0ddab949589e39b8c7c5b912a8d80295aec
tadm123 <drb_40@hotmail.com>
2024-10-24 19:36:35 -0400

    Create 153_MinRotatedSortedArray.cpp

---
+++ LeetCodeProblems/153_MinRotatedSortedArray.cpp
@@ -0,0 +1,37 @@
+/*
+Find Minimum in Rotated Sorted Array:
+Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+You must write an algorithm that rns in O(log n) time.
+*/
+
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) 
+    {
+        int res{nums[0]};
+        int l = 0;
+        int r = static_cast<int>(nums.size()) - 1;
+
+        while (l <= r)
+        {
+            if (nums[l] < nums[r])    //if array is already sorted as it is
+            {
+                res = std::min(res, nums[l]);      //get the left-most value 
+                break;
+            }
+            else
+            {
+                int m = (l + r)/2;
+                res = std::min(res, nums[m]);
+
+                if (nums[m] >= nums[l]) //if we're on the left sorted portion
+                    l = m + 1;
+                else                    //if we're on the right sorted portion
+                    r = m - 1;
+                
+            }
+        }
+        return res;
+    }
+};
