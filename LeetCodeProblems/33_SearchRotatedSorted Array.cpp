b31ee0fea2e07c3eb02cc610cb9795fc9e209dd8
tadm123 <drb_40@hotmail.com>
2024-12-17 02:06:37 -0500

    Create 33_SearchRotatedSorted Array.cpp

---
+++ LeetCodeProblems/33_SearchRotatedSorted Array.cpp
@@ -0,0 +1,35 @@
+/*
+Search in Rotated Sorted Array:
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums
+*/
+
+class Solution {
+public:
+    int search(std::vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
